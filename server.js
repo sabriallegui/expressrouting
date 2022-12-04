43905a02af207398def6fb3c07e133e863f87cb3
Sabri Allegui <sabri.allegui@vistaprint.com>
2022-12-05 00:09:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+
+//-----VARIABLES
+let t = new Date().getHours();
+
+app.use(express.static(__dirname + "/index"))
+app.set("view engine", hbs);
+
+//----GET
+if (t < 9 || t > 17) app.get("*", (req, res) => res.render("error.hbs"));
+else {
+
+
+  app.get("/contact/:name", (req, res) => {
+    res.render("contact.hbs", { name: req.params.name });
+  });
+}
+
+//---LISTEN
+app.listen(4001, err => {
+  if (err) console.log("Sorry!... Our office is not open now");
+  else console.log("Welcome to our office <3");
+});
